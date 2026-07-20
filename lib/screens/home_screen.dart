3e6e1a83b3d1fa5c05c9316e85758e8c08f89b53
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-07-21 01:46:11 +0500

    Display saved expenses on Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../models/expense.dart';
+import '../services/hive_service.dart';
 import 'add_expense_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,18 +10,58 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final box = HiveService.getBox();
+
     return Scaffold(
       appBar: AppBar(title: const Text("Expense Tracker"), centerTitle: true),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddExpenseScreen()),
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const AddExpenseScreen()),
+          );
+        },
+      ),
+
+      body: ValueListenableBuilder(
+        valueListenable: box.listenable(),
+        builder: (context, Box<Expense> box, _) {
+          if (box.isEmpty) {
+            return const Center(
+              child: Text(
+                "No expenses yet",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             );
-          },
-          child: const Text("Add Expense"),
-        ),
+          }
+
+          final expenses = box.values.toList().reversed.toList();
+
+          return ListView.builder(
+            itemCount: expenses.length,
+            itemBuilder: (context, index) {
+              final expense = expenses[index];
+
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  leading: const CircleAvatar(child: Icon(Icons.attach_money)),
+                  title: Text(expense.category),
+                  subtitle: Text("${expense.note}\n${expense.date.toLocal()}"),
+                  trailing: Text(
+                    "Rs ${expense.amount}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
